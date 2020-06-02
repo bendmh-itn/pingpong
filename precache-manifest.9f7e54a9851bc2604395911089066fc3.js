b650629a33fcce4ab9bfb084e6f2a8ec5eb3f7b2
bendmh <demab@site.asty-moulin.be>
2020-06-03 00:37:19 +0200

    Updates

---
+++ precache-manifest.9f7e54a9851bc2604395911089066fc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "714b299f0a206ad890d64bb0b5716900",
+    "url": "/pingpong/index.html"
+  },
+  {
+    "revision": "470a0628356b7a81e46d",
+    "url": "/pingpong/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cb19de454732a5b8a14b",
+    "url": "/pingpong/static/js/2.e3a654c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pingpong/static/js/2.e3a654c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "470a0628356b7a81e46d",
+    "url": "/pingpong/static/js/main.3320594e.chunk.js"
+  },
+  {
+    "revision": "c0647d25477fc0135ea4",
+    "url": "/pingpong/static/js/runtime-main.00742b54.js"
+  }
+]);
